bdfa980aa2d21cdea9221e51f499e3a5ad1f59d3
Joe Heck <heckj@mac.com>
2022-09-27 17:38:46 -0700

    app stub for explorivating

---
+++ VizToy/SolarSystemGenerator/SolarSystemGenerator/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  SolarSystemGenerator
+//
+//  Created by Joseph Heck on 9/27/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ VizToy/SolarSystemGenerator/SolarSystemGenerator/SolarSystemGeneratorApp.swift
@@ -0,0 +1,17 @@
+//
+//  SolarSystemGeneratorApp.swift
+//  SolarSystemGenerator
+//
+//  Created by Joseph Heck on 9/27/22.
+//
+
+import SwiftUI
+
+@main
+struct SolarSystemGeneratorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
